5e9ffadcec12d7baa0b76a783bf1004c463c75b8
Max Verba <maxwillow93@gmail.com>
2020-01-27 22:52:32 +0200

    Updates

---
+++ precache-manifest.7a372230a33c438f80f371d02932285f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a044dcba70f634e86d66f9acbefe1566",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "80b4c44b1c55230d7891",
+    "url": "/goit-react-hw-04-movies/static/css/main.0b3b89b5.chunk.css"
+  },
+  {
+    "revision": "f92195c343f0881fbf6e",
+    "url": "/goit-react-hw-04-movies/static/js/0.c7b80ce1.chunk.js"
+  },
+  {
+    "revision": "aadbeb2c7520b3468c1e",
+    "url": "/goit-react-hw-04-movies/static/js/8.a9a2e56c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-04-movies/static/js/8.a9a2e56c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdc854ed023580359384",
+    "url": "/goit-react-hw-04-movies/static/js/cast-page.7d1be001.chunk.js"
+  },
+  {
+    "revision": "494850747a27ffbee59e",
+    "url": "/goit-react-hw-04-movies/static/js/home-page.2ccfc31b.chunk.js"
+  },
+  {
+    "revision": "80b4c44b1c55230d7891",
+    "url": "/goit-react-hw-04-movies/static/js/main.7c398f1f.chunk.js"
+  },
+  {
+    "revision": "cd2961a3262b5da6e299",
+    "url": "/goit-react-hw-04-movies/static/js/movie-details-page.90c2bcb5.chunk.js"
+  },
+  {
+    "revision": "c12ddc230f600073f6b6",
+    "url": "/goit-react-hw-04-movies/static/js/movie-page.4d1631c6.chunk.js"
+  },
+  {
+    "revision": "9ad47b7f3661bdee601e",
+    "url": "/goit-react-hw-04-movies/static/js/reviews-page.47cb1cb6.chunk.js"
+  },
+  {
+    "revision": "99f7f6eb738b453b9b7d",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.38541687.js"
+  }
+]);
